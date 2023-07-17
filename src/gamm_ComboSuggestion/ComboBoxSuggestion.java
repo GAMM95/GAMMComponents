e7b57bc2c9de70962217d640126e27a5e6d07677
GAMM <jhonatanmm.1995@gmail.com>
2023-07-16 21:50:18 -0500

    add ComboBox Suggestion

---
+++ src/gamm_ComboSuggestion/ComboBoxSuggestion.java
@@ -0,0 +1,10 @@
+package gamm_ComboSuggestion;
+
+import javax.swing.JComboBox;
+
+public class ComboBoxSuggestion<E> extends JComboBox<E> {
+
+    public ComboBoxSuggestion() {
+        setUI(new ComboSuggestionUI());
+    }
+}
